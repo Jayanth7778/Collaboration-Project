b7b681b520f1757682e391fe4a66688610403540
Jayanth7778 <gundalajayanthkumargoud@gmail.com>
2017-12-19 14:38:04 +0530

    Notification Module

--- collaborationbackend/src/main/java/com/niit/configuration/DBConfiguration.java
+++ collaborationbackend/src/main/java/com/niit/configuration/DBConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.niit.model.BlogPost;
 import com.niit.model.Job;
+import com.niit.model.Notification;
 import com.niit.model.User;
 
 import java.util.Properties;
@@ -33,7 +34,7 @@ public class DBConfiguration
 		hibernateProperties.setProperty("hibernate.show_sql", "true");
 		lsf.addProperties(hibernateProperties);
 		
-		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class};
+		Class classes[]=new Class[]{User.class,Job.class,BlogPost.class,Notification.class};
 	    return lsf.addAnnotatedClasses(classes).buildSessionFactory();
 	}
 	
