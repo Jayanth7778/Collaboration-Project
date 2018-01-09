cfa6fdd369a27a7075274ccc06ade29042318212
Jayanth7778 <gundalajayanthkumargoud@gmail.com>
2018-01-09 17:56:22 +0530

    Notification Module

---
+++ collaborationfrontend/WebContent/controllers/homecontroller.js
@@ -0,0 +1,51 @@
+/**
+ *	HomeController 
+ */
+app.controller('HomeController',function($rootScope,$location,HomeService)
+		{
+			function getNotification()
+			{
+				HomeService.getNotificationNotViewed().then(function(response)
+						{
+							$rootScope.notificationNotViewed=response.data
+							$rootScope.notificationNotViewedLength=$rootScope.notificationNotViewed.length
+							alert($rootScope.notificationNotViewedLength)
+						},
+						function(response)
+						{
+							if(response.status==401)
+								$location.path('/login')
+						})
+				HomeService.getNotificationViewed().then(function(response)
+						{
+							$rootScope.notificationViewed=response.data
+						},
+						function(response)
+						{
+							if(response.status==401)
+								$location.path('/login')
+						})
+			}
+			
+			getNotification()
+			
+			
+			$rootScope.updateLength=function()
+			{
+				$rootScope.notificationNotViewedLength=0
+			}
+			
+			$rootScope.updateNotification=functionn(id)
+			{
+				HomeService.updateNotification(notificationId).then(function(response)
+						{
+							getNotification()
+						},
+						function(response)
+						{
+							if(response.status==401)
+								$location.path('/login')
+						})
+			}
+			
+		})
